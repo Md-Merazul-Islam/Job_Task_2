f677e1a87e456fe1286647b57a9b705e65bfb7ca
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-09 10:47:30 +0600

    Update URL configuration to serve media files in debug mode

--- PocketSense/urls.py
+++ PocketSense/urls.py
@@ -1,7 +1,3 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from django.urls import path, include
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -11,8 +7,8 @@
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
@@ -46,6 +42,8 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
